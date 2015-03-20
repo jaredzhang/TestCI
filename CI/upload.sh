3e65533db8d0f2615a71934297d06a89f8e9134e
Zhuodong Zhang <jaredzhang09@gmail.com>
2015-03-20 16:14:43 +0800

    update one more

--- CI/upload.sh
+++ CI/upload.sh
@@ -7,26 +7,17 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "This is a pull request. No deployment."
   exit 0
 fi
-if [ "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != "test" ]; then
-  echo "Building on a branch other than master and test. No deployment."
+if [[ "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != "develop" ]]; then
+  echo "Building on a branch other than master and develop. No deployment."
   exit 0
 fi
 if [ "$TRAVIS_REPO_SLUG" != "$REPO_SLUG" ]; then
   echo "It's not the main repo. No deployment."
 fi
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-  APP_ID="261"
-  PROFILE_NAME="InHouseWildcard"
-fi
 
 OUTPUTDIR="$PWD/app/build/outputs/apk"
 
-echo "$OUTPUTDIR"
-
-BUILD_NUMBER= $TRAVIS_BUILD_NUMBER
-VERSION_NUMBER= $TRAVIS_BUILD_NUMBER
-#VERSION="$VERSION_NUMBER ($BUILD_NUMBER) #$TRAVIS_BUILD_NUMBER"
 VERSION="#$TRAVIS_BUILD_NUMBER"
 
 DESCRIPTION=`git log --format=%B -n 1 HEAD`
