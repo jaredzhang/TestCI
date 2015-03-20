d71ea67fd40bec084101325ff52bc60eb0c35463
Zhuodong Zhang <jaredzhang09@gmail.com>
2015-03-20 15:55:56 +0800

    update again

--- CI/upload.sh
+++ CI/upload.sh
@@ -3,29 +3,32 @@
 # The script exits immediately if any statement or command returns non-true value
 set -e
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "This is a pull request. No deployment."
   exit 0
 fi
-if [[ "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != "test" ]]; then
+if [ "$TRAVIS_BRANCH" != "master" && "$TRAVIS_BRANCH" != "test" ]; then
   echo "Building on a branch other than master and test. No deployment."
   exit 0
 fi
-if [[ "$TRAVIS_REPO_SLUG" != "$REPO_SLUG" ]]; then
+if [ "$TRAVIS_REPO_SLUG" != "$REPO_SLUG" ]; then
   echo "It's not the main repo. No deployment."
 fi
 
-if [[ "$TRAVIS_BRANCH" == "master" ]]; then
+if [ "$TRAVIS_BRANCH" == "master" ]; then
   APP_ID="261"
   PROFILE_NAME="InHouseWildcard"
 fi
 
 OUTPUTDIR="$PWD/app/build/outputs/apk"
 
-echo OUTPUTDIR
+echo "$OUTPUTDIR"
 
 BUILD_NUMBER= $TRAVIS_BUILD_NUMBER
 VERSION_NUMBER= $TRAVIS_BUILD_NUMBER
+#VERSION="$VERSION_NUMBER ($BUILD_NUMBER) #$TRAVIS_BUILD_NUMBER"
+VERSION="#$TRAVIS_BUILD_NUMBER"
+
 DESCRIPTION=`git log --format=%B -n 1 HEAD`
 
 if [ ! -z "$APP_MANAGER_API_TOKEN" ]; then
@@ -33,7 +36,6 @@ if [ ! -z "$APP_MANAGER_API_TOKEN" ]; then
   echo "************************************************************************"
   echo "*             Uploading to 2359 Media Enterprise App Manager           *"
   echo "************************************************************************"
-  VERSION="$VERSION_NUMBER ($BUILD_NUMBER) #$TRAVIS_BUILD_NUMBER"
   curl https://app.2359media.net/api/v1/apps/$APP_ID/versions \
     -F binary="@$OUTPUTDIR/app-debug.apk" \
     -F api_token="$APP_MANAGER_API_TOKEN" \
